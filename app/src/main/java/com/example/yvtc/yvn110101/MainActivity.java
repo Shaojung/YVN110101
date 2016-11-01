e19a5baaa0a0e46670415e35c334e8c784f7adef
shaojung <shaojung@gmail.com>
2016-11-01 20:22:16 +0800

    add EditText

--- app/src/main/java/com/example/yvtc/yvn110101/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110101/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -48,12 +49,14 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("輸入對話框測試");
         builder.setMessage("請輸入你的暱稱");
-        EditText ed = new EditText(MainActivity.this);
+        final EditText ed = new EditText(MainActivity.this);
         builder.setView(ed);
         builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                TextView tv2 = (TextView) findViewById(R.id.textView2);
+                String str = ed.getText().toString();
+                tv2.setText(str);
             }
         });
 
