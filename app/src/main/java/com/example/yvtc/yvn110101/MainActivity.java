a5cd58808bba01ae414b180c1740024093b75ca6
shaojung <shaojung@gmail.com>
2016-11-01 20:02:29 +0800

    add EditText

--- app/src/main/java/com/example/yvtc/yvn110101/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110101/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,9 +41,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "忽略被按下", Toast.LENGTH_SHORT).show();
             }
         });
+        builder.show();
+    }
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入對話框測試");
+        builder.setMessage("請輸入你的暱稱");
+        EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
+            }
+        });
 
         builder.show();
-
     }
 }
